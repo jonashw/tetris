e2a8a4ebd9d7945a3021c7efa1a5c01dadda66ec
jonashw <2jonwilson@gmail.com>
2013-01-18 00:58:36 -0800

    ActivePiece now an Observable, firing events: 'new','drop','move':direction,'rotate':direction.  ActivePiece.drop() now delegates the query of drop distance to ActivePiece.getDropDelta()... to be used in shadow rendering...

--- ActivePiece.js
+++ ActivePiece.js
@@ -1,7 +1,15 @@
 function ActivePiece(board,piece){
 	this.board = board;
 	this.piece = piece;//optional
+	new Observable(this);
 }
+ActivePiece.prototype.setPiece = function(piece){
+	this.piece = piece;
+	this.notifyObservers('new');
+};
+ActivePiece.prototype.getPiece = function(){
+	return this.piece;
+};
 ActivePiece.prototype.canDrop = function(){
 	return !!this.piece;
 };
@@ -26,7 +34,7 @@ ActivePiece.prototype.canMoveDown = function(){
 ActivePiece.prototype.canRotateCW = function(){
 	var board = this.board;
 	var verts = this.piece.getNextCWVertices();
-	console.log(verts);
+	//console.log(verts);
 	return !!this.piece && verts.every(function(vert){
 		return board.hasEmptyCellAt(vert[0],vert[1]);
 	});
@@ -34,7 +42,7 @@ ActivePiece.prototype.canRotateCW = function(){
 ActivePiece.prototype.canRotateCCW = function(){
 	var board = this.board;
 	var verts = this.piece.getNextCCWVertices();
-	console.log(verts);
+	//console.log(verts);
 	return !!this.piece && verts.every(function(vert){
 		return board.hasEmptyCellAt(vert[0],vert[1]);
 	});
@@ -42,12 +50,19 @@ ActivePiece.prototype.canRotateCCW = function(){
 ActivePiece.prototype.moveLeft = function(){
 	if(!this.canMoveLeft()) return false;
 	this.piece.x--;
+	this.notifyObservers('move','left');
 };
 ActivePiece.prototype.moveRight = function(){
 	if(!this.canMoveRight()) return false;
 	this.piece.x++;
+	this.notifyObservers('move','right');
 };
 ActivePiece.prototype.drop = function(){
+	if(!this.canDrop()) return false;
+	this.piece.y += this.getDropDelta();
+	this.notifyObservers('drop');
+};
+ActivePiece.prototype.getDropDelta = function(){
 	if(!this.canDrop()) return false;
 	var verts = this.piece.getVertices();
 	var board = this.board;
@@ -55,17 +70,20 @@ ActivePiece.prototype.drop = function(){
 	while(verts.every(function(vert){return board.hasEmptyCellAt(vert[0], vert[1] + i-1);})){
 		i--;
 	}
-	this.piece.y += i;
+	return i;
 };
 ActivePiece.prototype.moveDown = function(){
 	if(!this.canMoveDown()) return false;
 	this.piece.y--;
+	this.notifyObservers('move','down');
 };
 ActivePiece.prototype.rotateCW = function(){
 	if(!this.canRotateCW()) return false;
 	this.piece.rotateCW();
+	this.notifyObservers('rotate','cw');
 };
 ActivePiece.prototype.rotateCCW = function(){
 	if(!this.canRotateCCW()) return false;
 	this.piece.rotateCCW();
+	this.notifyObservers('rotate','ccw');
 };
