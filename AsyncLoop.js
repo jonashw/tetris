323e9fbba53ce3af63b50e11438707b4f68beb6e
jonashw <2jonwilson@gmail.com>
2013-01-18 03:52:58 -0800

    style tweak in AsyncLoop

--- AsyncLoop.js
+++ AsyncLoop.js
@@ -1,18 +1,15 @@
-function AsyncLoop(loopBody, delay, beforeStart, afterStop){
-	this.loopBody = loopBody;
+function AsyncLoop(fn, delay){
+	this.fn = fn;
 	this.delay = delay;
-	this.beforeStart = beforeStart;
-	this.afterStop = afterStop;
 	this.timeout = null;
 	this.isLooping = false;
 }
 AsyncLoop.prototype.start = function(){
-	if(this.beforeStart) this.beforeStart();
 	this.isLooping = true;
 	this.execute();
 };
 AsyncLoop.prototype.execute = function(){
-	this.loopBody();
+	this.fn();
 	var self = this;
 	if(this.isLooping) this.timeout = setTimeout(function(){
 		self.execute();
@@ -21,7 +18,6 @@ AsyncLoop.prototype.execute = function(){
 AsyncLoop.prototype.stop = function(){
 	clearTimeout(this.timeout);
 	this.isLooping = false;
-	if(this.afterStop) this.afterStop();
 };
 AsyncLoop.prototype.isRunning = function(){
 	return this.isLooping;
